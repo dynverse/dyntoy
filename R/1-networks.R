8788972aea40624be5b68aca77ea4dce3e720bff
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-24 10:46:33 +0200

    added ba graph

--- R/1-networks.R
+++ R/1-networks.R
@@ -1,11 +1,74 @@
-generate_toy_milestone_network <- function(ti_type=c("linear", "bifurcating", "cycle")) {
-  if(!(ti_type %in% c("linear", "bifurcating", "cycle"))) stop("invalid ti_type")
+#' @importFrom igraph ba.game
+#' @importFrom stats runif
+generate_toy_milestone_network <- function(ti_type = c("linear", "bifurcating", "cycle", "consecutive_bifurcating", "trifurcating", "converging", "BA")) {
+  requireNamespace("igraph")
+  ti_type <- arg.match(ti_type)
 
-  if(identical(ti_type, "linear")) {
-    tibble(from=c("M1"), to=c("M2"), length=1, directed=TRUE)
-  } else if (identical(ti_type, "bifurcating")) {
-    tibble(from=c("M1", "M2", "M2", "M3", "M4"), to=c("M2", "M3", "M4", "M5", "M6"), length=1, directed=TRUE)
-  } else if (identical(ti_type, "cycle")) {
-    tibble(from=c("M1", "M2", "M3"), to=c("M2", "M3", "M1"), length=1, directed=TRUE)
+  milnet <- switch(
+    ti_type,
+    linear = {
+      tribble(
+        ~from, ~to,
+        "M1", "M2"
+      )
+    },
+    bifurcating = {
+      tribble(
+        ~from, ~to, ~length,
+        "M1", "M2", 3,
+        "M2", "M3", 2,
+        "M2", "M4", 2,
+        "M3", "M5", 1,
+        "M4", "M6", 1
+      )
+    },
+    trifurcating = {
+      tribble(
+        ~from, ~to,
+        "M1", "M2", 3,
+        "M2", "M3", 2,
+        "M2", "M4", 2,
+        "M2", "M5", 2,
+        "M3", "M6", 1,
+        "M4", "M7", 1,
+        "M5", "M8", 1
+      )
+    },
+    consecutive_bifurcating = tribble(
+      ~from, ~to,
+      "M1", "M2",
+      "M2", "M3",
+      "M3", "M4",
+      "M3", "M5",
+      "M2", "M6"
+    ),
+    converging = tribble(
+      ~from, ~to,
+      "M1", "M2",
+      "M3", "M4",
+      "M2", "M5",
+      "M4", "M5",
+      "M5", "M6"
+    ),
+    cycle = {
+      tribble(
+        ~from, ~to,
+        "M1", "M2",
+        "M2", "M3",
+        "M3", "M1"
+      )
+    },
+    BA = {
+      num_milestones <- round(stats::runif(1, 5, 15))
+      igraph::ba.game(num_milestones) %>%
+        igraph::as_data_frame() %>%
+        mutate_at(c("from", "to"), function(x) paste0("M", x)) %>%
+        select(from = to, to = from)
+    }
+  )
+
+  if (!"length" %in% colnames(milnet)) {
+    milnet <- milnet %>% mutate(length = 1)
   }
+  milnet %>% mutate(directed = T)
 }
